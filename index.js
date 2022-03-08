f7ceec44362042d86c54a1ee84ab4891e97f6fa9
Ankur Panchani <ankurpanchani.career@gmail.com>
2022-03-08 22:25:54 +0530

    v1.2.0: bump dependencies and fix web warnings

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import {getLinkPreview} from 'link-preview-js';
-import PropTypes from 'prop-types';
-import {Image, Linking, Platform, Text, TouchableOpacity, View, ViewPropTypes} from 'react-native';
+import { getLinkPreview } from "link-preview-js";
+import PropTypes from "prop-types";
+import React from "react";
+import {
+  Image,
+  Linking,
+  Platform,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-const REGEX = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/g;
+const REGEX =
+  /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/g;
 
 export default class RNUrlPreview extends React.PureComponent {
   constructor(props) {
@@ -19,9 +27,9 @@ export default class RNUrlPreview extends React.PureComponent {
   }
 
   getPreview = (text, options) => {
-    const {onError, onLoad} = this.props;
+    const { onError, onLoad } = this.props;
     getLinkPreview(text, options)
-      .then(data => {
+      .then((data) => {
         onLoad(data);
         this.setState({
           isUri: true,
@@ -29,16 +37,23 @@ export default class RNUrlPreview extends React.PureComponent {
           linkDesc: data.description ? data.description : undefined,
           linkImg:
             data.images && data.images.length > 0
-              ? data.images.find(function(element) {
-                  return element.includes('.png') || element.includes('.jpg') || element.includes('.jpeg');
+              ? data.images.find(function (element) {
+                  return (
+                    element.includes(".png") ||
+                    element.includes(".jpg") ||
+                    element.includes(".jpeg")
+                  );
                 })
               : undefined,
-          linkFavicon: data.favicons && data.favicons.length > 0 ? data.favicons[data.favicons.length - 1] : undefined,
+          linkFavicon:
+            data.favicons && data.favicons.length > 0
+              ? data.favicons[data.favicons.length - 1]
+              : undefined,
         });
       })
-      .catch(error => {
+      .catch((error) => {
         onError(error);
-        this.setState({isUri: false});
+        this.setState({ isUri: false });
       });
   };
 
@@ -46,7 +61,7 @@ export default class RNUrlPreview extends React.PureComponent {
     if (nextProps.text !== this.props.text) {
       this.getPreview(nextProps.text);
     } else if (nextProps.text == null) {
-      this.setState({isUri: false});
+      this.setState({ isUri: false });
     }
   }
 
@@ -54,14 +69,34 @@ export default class RNUrlPreview extends React.PureComponent {
     Linking.openURL(this.props.text.match(REGEX)[0]);
   };
 
-  renderImage = (imageLink, faviconLink, imageStyle, faviconStyle, imageProps) => {
+  renderImage = (
+    imageLink,
+    faviconLink,
+    imageStyle,
+    faviconStyle,
+    imageProps
+  ) => {
     return imageLink ? (
-      <Image style={imageStyle} source={{uri: imageLink}} {...imageProps} />
+      <Image style={imageStyle} source={{ uri: imageLink }} {...imageProps} />
     ) : faviconLink ? (
-      <Image style={faviconStyle} source={{uri: faviconLink}} {...imageProps} />
+      <Image
+        style={faviconStyle}
+        source={{ uri: faviconLink }}
+        {...imageProps}
+      />
     ) : null;
   };
-  renderText = (showTitle, showDescription, title, description, textContainerStyle, titleStyle, descriptionStyle, titleNumberOfLines, descriptionNumberOfLines) => {
+  renderText = (
+    showTitle,
+    showDescription,
+    title,
+    description,
+    textContainerStyle,
+    titleStyle,
+    descriptionStyle,
+    titleNumberOfLines,
+    descriptionNumberOfLines
+  ) => {
     return (
       <View style={textContainerStyle}>
         {showTitle && (
@@ -70,7 +105,10 @@ export default class RNUrlPreview extends React.PureComponent {
           </Text>
         )}
         {showDescription && (
-          <Text numberOfLines={descriptionNumberOfLines} style={descriptionStyle}>
+          <Text
+            numberOfLines={descriptionNumberOfLines}
+            style={descriptionStyle}
+          >
             {description}
           </Text>
         )}
@@ -92,19 +130,38 @@ export default class RNUrlPreview extends React.PureComponent {
     descriptionStyle,
     titleNumberOfLines,
     descriptionNumberOfLines,
-    imageProps,
+    imageProps
   ) => {
     return (
-      <TouchableOpacity style={[styles.containerStyle, containerStyle]} activeOpacity={0.9} onPress={() => this._onLinkPressed()}>
-        {this.renderImage(imageLink, faviconLink, imageStyle, faviconStyle, imageProps)}
-        {this.renderText(showTitle, showDescription, title, description, textContainerStyle, titleStyle, descriptionStyle, titleNumberOfLines, descriptionNumberOfLines)}
+      <TouchableOpacity
+        style={[styles.containerStyle, containerStyle]}
+        activeOpacity={0.9}
+        onPress={() => this._onLinkPressed()}
+      >
+        {this.renderImage(
+          imageLink,
+          faviconLink,
+          imageStyle,
+          faviconStyle,
+          imageProps
+        )}
+        {this.renderText(
+          showTitle,
+          showDescription,
+          title,
+          description,
+          textContainerStyle,
+          titleStyle,
+          descriptionStyle,
+          titleNumberOfLines,
+          descriptionNumberOfLines
+        )}
       </TouchableOpacity>
     );
   };
 
   render() {
     const {
-      text,
       containerStyle,
       imageStyle,
       faviconStyle,
@@ -133,7 +190,7 @@ export default class RNUrlPreview extends React.PureComponent {
           descriptionStyle,
           titleNumberOfLines,
           descriptionNumberOfLines,
-          imageProps,
+          imageProps
         )
       : null;
   }
@@ -141,7 +198,7 @@ export default class RNUrlPreview extends React.PureComponent {
 
 const styles = {
   containerStyle: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
 };
 
@@ -151,8 +208,8 @@ RNUrlPreview.defaultProps = {
   text: null,
   requestOptions: {},
   containerStyle: {
-    backgroundColor: 'rgba(239, 239, 244,0.62)',
-    alignItems: 'center',
+    backgroundColor: "rgba(239, 239, 244,0.62)",
+    alignItems: "center",
   },
   imageStyle: {
     width: Platform.isPad ? 160 : 110,
@@ -168,47 +225,51 @@ RNUrlPreview.defaultProps = {
   },
   textContainerStyle: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
     padding: 10,
   },
   title: true,
   description: true,
   titleStyle: {
     fontSize: 17,
-    color: '#000',
+    color: "#000",
     marginRight: 10,
     marginBottom: 5,
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
   },
   titleNumberOfLines: 2,
   descriptionStyle: {
     fontSize: 14,
-    color: '#81848A',
+    color: "#81848A",
     marginRight: 10,
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
   },
   descriptionNumberOfLines: Platform.isPad ? 4 : 3,
-  imageProps: {resizeMode: 'contain'},
+  imageProps: { resizeMode: "contain" },
 };
 
 RNUrlPreview.propTypes = {
   onLoad: PropTypes.func,
   onError: PropTypes.func,
   text: PropTypes.string,
-  containerStyle: ViewPropTypes ? ViewPropTypes.style : PropTypes.object,
-  imageStyle: ViewPropTypes ? ViewPropTypes.style : PropTypes.object,
-  faviconStyle: ViewPropTypes ? ViewPropTypes.style : PropTypes.object,
-  textContainerStyle: ViewPropTypes ? ViewPropTypes.style : PropTypes.object,
+  containerStyle: PropTypes.object,
+  imageStyle: PropTypes.object,
+  faviconStyle: PropTypes.object,
+  textContainerStyle: PropTypes.object,
   title: PropTypes.bool,
   description: PropTypes.bool,
   titleStyle: Text.propTypes ? Text.propTypes.style : PropTypes.object,
-  titleNumberOfLines: Text.propTypes ? Text.propTypes.numberOfLines : PropTypes.number,
+  titleNumberOfLines: Text.propTypes
+    ? Text.propTypes.numberOfLines
+    : PropTypes.number,
   descriptionStyle: Text.propTypes ? Text.propTypes.style : PropTypes.object,
-  descriptionNumberOfLines: Text.propTypes ? Text.propTypes.numberOfLines : PropTypes.number,
+  descriptionNumberOfLines: Text.propTypes
+    ? Text.propTypes.numberOfLines
+    : PropTypes.number,
   requestOptions: PropTypes.shape({
     headers: PropTypes.objectOf(PropTypes.string),
     imagesPropertyType: PropTypes.string,
-    proxyUrl: PropTypes.string
-  })
+    proxyUrl: PropTypes.string,
+  }),
 };
